2083a66a11c6defb1a941313806801695dcff342
Adventurous Swine <adventurousswine@gmail.com>
2022-06-06 21:56:57 -0400

    start up adjustments (work in progress)

--- password.py
+++ password.py
@@ -8,19 +8,36 @@ filename = "passwords.txt"
 passdict = {}
 random.seed(secrets.token_bytes())
 
-def startup():
-    input("Would you like to create a new master password? y/n")
+
+class Master:
+
+    def __init__(self, mp: str):
+        self.mstr = mp
+
+
+def initial():
+
+    mp = input("Please create your new master password: ")
+    return mode()
+
+
+def subsequent():
+
+    input("Would you like to update your master password? y/n")
     if input == "y":
-        global mstr
-        mstr = input("Please create your password: ")
-        return mode()
+        input("Create your new password:")
     else:
-        input("What is the master password? ")
-        if input != mstr:
-            print("The password is incorrect. Please try again. ")
-            return
-        else:
-            return mode()
+        return old()
+
+
+def old():
+
+    input("What is the master password? ")
+    if input != Master:
+        print("The password is incorrect. Please try again. ")
+        return
+    else:
+        return mode()
 
 
 
@@ -28,15 +45,18 @@ alphabet = string.ascii_letters + string.digits
 password = ''.join(secrets.choice(alphabet) for i in range(8, 18))
 work = random.randint(4, 8)
 
+
 def gen():
     scret = str(secrets.token_hex(work))
     print(f"{scret} has {len(scret)} characters.")
+
 
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
     write_key()
+
 
 def load_key():
     file = open("key.key", "rb")
@@ -47,14 +67,18 @@ def load_key():
 #key = load_key() + mstr.encode()
 #fer = Fernet(key)
 
+
 def view():
+
     with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = (line.rstrip())
             usrn, pwd, email = data.split("|")
-            print("Username: ", usern, "Email: ", email, "Password: ", fer.decrypt(pwd.encode()))
+            print("Username: ", usrn, "Email: ", email, "Password: ", fer.decrypt(pwd.encode()))
+
 
 def add():
+
     confirm_input = input("Would you like to generate a password? y/n")
     if confirm_input == "y":
         gen()
@@ -76,18 +100,16 @@ def add():
     json.dump(passdict, out_file, indent=4, sort_keys=False)
     out_file.close()
 
-def mode():
-    input("Would you like to add a new account or view existing ones? (add, view, quit)")
 
+def mode():
+
+    input("Would you like to add a new account or view existing ones? (add, view, quit)")
     if mode == "quit":
         quit()
-
     if mode == "view":
         view()
-
     if mode == "add":
         add()
-
     else:
         print("Invalid input.")
         mode()
